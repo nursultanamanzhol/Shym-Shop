0e88d382b41130b06e66239bf024f55dddfc2b0b
Nursultan <nursamanzhol@gmail.com>
2023-07-13 16:52:14 +0600

    do desing for login

--- app/src/main/java/com/example/bookapp/SplashActivity.kt
+++ app/src/main/java/com/example/bookapp/SplashActivity.kt
@@ -1,11 +1,17 @@
 package com.example.bookapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed(Runnable{
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 2000) //means 2 seconds
     }
 }
