3aae28fcbfa49ff36b742dfb44ced242077e8fe1
Nursultan <nursamanzhol@gmail.com>
2023-07-13 18:16:23 +0600

    connect to Firebase

--- app/src/main/java/com/example/bookapp/MainActivity.kt
+++ app/src/main/java/com/example/bookapp/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.bookapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bookapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //view binding
+    private lateinit var binding:ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //handle click, login
+        binding.loginBtn.setOnClickListener{
+            //will do later
+        }
+
+        //hand click, skip and continue to main screen
+        binding.skipBtn.setOnClickListener {
+            //will do later
+        }
+
+        //now lets connect with firebase
+
     }
 }
