b4d90d058a45e12e884f7d0feaff1abba5958776
Nursultan <nursamanzhol@gmail.com>
2023-08-20 11:45:46 +0600

    20.08.2023 go on the project

--- app/src/main/java/com/example/bookapp/DashboardAdminActivity.kt
+++ app/src/main/java/com/example/bookapp/DashboardAdminActivity.kt
@@ -2,10 +2,21 @@ package com.example.bookapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bookapp.databinding.ActivityDashboardAdminBinding
 
 class DashboardAdminActivity : AppCompatActivity() {
+
+    //view binding
+    private lateinit var binding : ActivityDashboardAdminBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard_admin)
+        binding = ActivityDashboardAdminBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
     }
 }
